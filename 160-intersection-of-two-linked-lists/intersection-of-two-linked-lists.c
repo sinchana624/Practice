617721bb1b8bdd5226f5ef9a670fa796277da639
Sinchana Hebbar K M <sinchanakmhebbar@gmail.com>
2026-08-16 23:25:16 +0530

    Time: 64 ms (65.94%) | Memory: 23.4 MB (56.91%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.c
@@ -0,0 +1,18 @@
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    struct ListNode *p = headA;
+    struct ListNode *q = headB;
+
+    while (p != q) {
+        if (p == NULL)
+            p = headB;
+        else
+            p = p->next;
+
+        if (q == NULL)
+            q = headA;
+        else
+            q = q->next;
+    }
+
+    return p;
+}
